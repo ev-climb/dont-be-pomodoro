901fbd5a91073d3f278b99e7e359679612efc2dd
evseev <evgeny.evseev@pimpay.ru>
2024-12-14 10:00:00 +0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'), // Настройка пути для @
     },
   },
+  base: '/ev-climb.github.io/',
   build: {
     outDir: "docs" 
   }
